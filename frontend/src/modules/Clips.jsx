ea78520c01514d66bbda2981a541a0792c7c919e
moran991231 <moran991231@naver.com>
2022-04-13 01:48:05 +0900

    apply eslint

--- frontend/src/modules/Clips.jsx
+++ frontend/src/modules/Clips.jsx
@@ -1,36 +1,39 @@
 import React from "react";
-import '../styles/Clips.css'
+import "../styles/Clips.css";
 
 // eslint-disable-next-line react/prop-types
-export function Clip({title, link_enabled, link, color_idx}){
-    const colors=['green','yellow', 'deep-green',  'beige']
-    const name = "clip "+colors[color_idx]
-    var clip_text = <div />
-    if (link_enabled)
-        clip_text = <a className="clip-text" href={link}> {title}</a>
-    else 
-        clip_text = <div className="clip-text"> {title}</div>
-        
+export function Clip({ title, link_enabled, link, color_idx }) {
+  const colors = ["green", "yellow", "deep-green", "beige"];
 
-    return (
-        <div className={name}>
-            {clip_text}
-        </div>
-    )
+  var clip_text = <div />;
+  if (link_enabled)
+    clip_text = (
+      <a className="clip-text" href={link}>
+        {" "}
+        {title}
+      </a>
+    );
+  else clip_text = <div className="clip-text"> {title}</div>;
 
+  return <div className={"clip " + colors[color_idx]}>{clip_text}</div>;
 }
 
 // eslint-disable-next-line react/prop-types
-export function ClipTray({clips}){
-
-    return (
-        <div className="clip-tray">
-            {
-            // eslint-disable-next-line react/prop-types
-            clips.map((clip)=>(
-                <Clip key = {clip} title={clip.title} link_enabled={clip.link_enabled} link = {clip.link} color_idx ={clip.color_idx} />
-            ))}
-        </div>
-    )
-
+export function ClipTray({ clips }) {
+  return (
+    <div className="clip-tray">
+      {
+        // eslint-disable-next-line react/prop-types
+        clips.map((clip) => (
+          <Clip
+            key={clip}
+            title={clip.title}
+            link_enabled={clip.link_enabled}
+            link={clip.link}
+            color_idx={clip.color_idx}
+          />
+        ))
+      }
+    </div>
+  );
 }
--- frontend/src/modules/Surveys.jsx
+++ frontend/src/modules/Surveys.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import '../styles/Surveys.css'
+import React from "react";
+import "../styles/Surveys.css";
 
 // eslint-disable-next-line react/prop-types
-export function SurveyIcon({title, color_idx, link}){
-    const colors=['green', 'yellow', 'deep-green ']
-    var color_box_name = "color-box " + colors[color_idx]
+export function SurveyIcon({ title, color_idx, link }) {
+  const colors = ["green", "yellow", "deep-green "];
+  var color_box_name = "color-box " + colors[color_idx];
 
-    return(
-        <a href={link} title={title}> 
-            <div className={color_box_name}>
-                <div className="white-box">
-                    {title}
-                </div>
-            </div>
-        </a>
-    )
-
-} 
+  return (
+    <a href={link} title={title}>
+      <div className={color_box_name}>
+        <div className="white-box">{title}</div>
+      </div>
+    </a>
+  );
+}
 
 // eslint-disable-next-line react/prop-types
-export  function SurveyIconsTray({open_surveys, color_idx}){
-    return(
-        <div className='tray'>
-            
-            {// eslint-disable-next-line react/prop-types
-            open_surveys.map((survey) => <SurveyIcon key = {survey} title={survey.title} color_idx = {color_idx} link ={survey.link} />
-            )
-            }
-        </div>
-    )
-
+export function SurveyIconsTray({ open_surveys, color_idx }) {
+  return (
+    <div className="tray">
+      {
+        // eslint-disable-next-line react/prop-types
+        open_surveys.map((survey) => (
+          <SurveyIcon
+            key={survey}
+            title={survey.title}
+            color_idx={color_idx}
+            link={survey.link}
+          />
+        ))
+      }
+    </div>
+  );
 }
