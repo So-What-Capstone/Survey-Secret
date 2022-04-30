ed0f80f588cc7011f2f928388086f0e135da916a
Jisuk Oh <muxacarin5@gmail.com>
2022-04-30 22:01:59 +0900

    feat: add Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import "../styles/Login.css";
 
 function Login() {
   return (
-    <div>
-      <h1>Implement login here...</h1>
+    <div className="login">
+      <div>
+        <h1>Survey Secret</h1>
+      </div>
+      <div>
+        <input type="id" placeholder="ID" className="loginInputField" />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="pw"
+          id="pw"
+          placeholder="Password"
+          className="loginInputField"
+        />
+      </div>
+      <div className="loginSubmit">
+        <input type="button" value="Login" className="loginSubmit" />
+      </div>
+      <div className="loginOther">
+        <input type="button" value="회원가입" />
+        <input type="button" value="PW 찾기" />
+      </div>
     </div>
   );
 }
