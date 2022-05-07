1858d5c0a508c3a64737485f779b24b47e9e148a
KoDaHyeon <wus2363@gmail.com>
2022-05-07 17:22:24 +0900

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,6 +5,7 @@ function Register() {
   //중복 제출 방지
   const [disabled, setDisabled] = useState(false);
 
+  /* 회원가입 정보 */
   const [id, setId] = useState("");
   const [pwd, setPwd] = useState("");
   const [pwdConfirm, setPwdConfirm] = useState("");
@@ -14,92 +15,98 @@ function Register() {
   const [idChecked, setIdChecked] = useState(false);
   const [emailChecked, setEmailChecked] = useState(false);
 
-  const [idError, setIdError] = useState("");
   const [idMessage, setIdMessage] = useState("");
-  const [pwdError, setPwdError] = useState("");
-  const [pwdConfirmError, setPwdConfirmError] = useState("");
+  const [pwdError, setPwdError] = useState(false);
+  const [pwdConfirmError, setPwdConfirmError] = useState(false);
+  const [pwdMessage, setPwdMessage] = useState("");
   const [emailMessage, setEmailMessage] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [nameError, setNameError] = useState("");
+  const [nameError, setNameError] = useState(false);
 
   const handleIdChange = (e) => {
     setId(e.target.value);
     setIdChecked(false);
     if (e.target.value.length < 8) {
-      setIdError("8자리 이상 입력해주세요.");
+      setIdMessage("8자리 이상 입력하세요.");
     } else {
-      setIdError("");
+      setIdMessage("");
     }
   };
 
   const handlePwdChange = (e) => {
     setPwd(e.target.value);
+    setPwdError(e.target.value.length < 8);
     if (e.target.value.length < 8) {
-      setPwdError("8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.");
+      setPwdError(true);
+      setPwdMessage("8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.");
     } else {
-      setPwdError("");
+      setPwdError(false);
     }
   };
 
   const handlePwdConfirmChange = (e) => {
     setPwdConfirm(e.target.value);
-    if (pwd !== e.target.value) {
-      setPwdConfirmError("비밀번호가 일치하지 않습니다.");
-    } else {
-      setPwdConfirmError("");
-    }
+    setPwdConfirmError(e.target.value !== pwd);
   };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
     setEmailChecked(false);
-    if (!setEmailChecked) {
-      setEmailMessage("");
-    }
+    setEmailMessage("");
   };
 
   const handleNameChange = (e) => {
     setName(e.target.value);
-    if (e.target.value.length > 0) {
-      setNameError("");
-    }
+    setNameError(false);
   };
 
   const idCheck = (e) => {
     //if id is not duplicate && id가 양식에 맞을 때
     setIdChecked(true);
-    setIdMessage("이용 가능한 ID입니다.");
   };
+
   const emailCheck = () => {
     //if email is checked
     setEmailChecked(true);
-    setEmailMessage("인증되었습니다.");
   };
+
   const onSubmit = async (e) => {
     setDisabled(true);
     e.preventDefault();
+    let canSubmit = true;
+
     if (pwd !== pwdConfirm) {
-      setPwdConfirmError("비밀번호가 일치하지 않습니다.");
+      setPwdConfirmError(true);
+      canSubmit = false;
     }
     if (!emailChecked) {
-      setEmailError("이메일 인증이 완료되지 않았습니다.");
+      setEmailMessage("이메일 인증이 완료되지 않았습니다.");
+      canSubmit = false;
     }
     if (!idChecked) {
-      setIdError("ID 중복 확인이 완료되지 않았습니다.");
+      setIdMessage("ID 중복 확인이 완료되지 않았습니다.");
+      canSubmit = false;
     }
     if (name.length < 1) {
-      setNameError("필수 정보입니다.");
+      setNameError(true);
+      canSubmit = false;
     }
     if (pwd.length < 1) {
-      setPwdError("필수 정보입니다.");
+      setPwdError(true);
+      setPwdMessage("필수 정보입니다.");
+      canSubmit = false;
     }
+
+    if (canSubmit) {
+      console.log(id + "," + pwd + "," + email + "," + name);
+      //register logic
+    }
+
     await new Promise((r) => setTimeout(r, 1000));
     setDisabled(false);
     /*To Do
     1. 중복제출방지 ㅇ
     2. 양식 검사
     3. 이전 이메일 같은거 불러오면 field가 이상해짐
-    4. 제출메소드 완성
     5. 메세지 없어져도 크기 일정하도록
     6. 창크기 줄여도 세로 위치 유지하도록
     */
@@ -127,8 +134,8 @@ function Register() {
             onClick={idCheck}
           />
         </div>
-        {!idChecked && <div className="error-box">{idError}</div>}
-        {idChecked && <div className="message-box">{idMessage}</div>}
+        {!idChecked && <div className="error-box">{idMessage}</div>}
+        {idChecked && <div className="message-box">이용가능한 ID입니다.</div>}
       </div>
       <div className="input-row">
         <label htmlFor="pwd">비밀번호</label>
@@ -141,7 +148,7 @@ function Register() {
             className="input-field"
           />
         </div>
-        <div className="error-box">{pwdError}</div>
+        {pwdError && <div className="error-box">{pwdMessage}</div>}
       </div>
       <div className="input-row">
         <label htmlFor="pwdCheck">비밀번호 확인</label>
@@ -154,7 +161,9 @@ function Register() {
             className="input-field"
           />
         </div>
-        <div className="error-box">{pwdConfirmError}</div>
+        {pwdConfirmError && (
+          <div className="error-box">비밀번호가 일치하지 않습니다.</div>
+        )}
       </div>
       <div className="input-row">
         <label htmlFor="name">이름</label>
@@ -167,7 +176,7 @@ function Register() {
             className="input-field"
           />
         </div>
-        <div className="error-box">{nameError}</div>
+        {nameError && <div className="error-box">필수 정보입니다.</div>}
       </div>
       <div className="input-row">
         <label htmlFor="email">이메일</label>
@@ -186,8 +195,8 @@ function Register() {
             onClick={emailCheck}
           />
         </div>
-        {!emailChecked && <div className="error-box">{emailError}</div>}
-        {emailChecked && <div className="message-box">{emailMessage}</div>}
+        {!emailChecked && <div className="error-box">{emailMessage}</div>}
+        {emailChecked && <div className="message-box">인증되었습니다.</div>}
       </div>
       <div>
         <input
