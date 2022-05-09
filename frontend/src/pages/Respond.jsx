632143c120e51ac57bf0b350a326851598f973e7
moran991231 <moran991231@naver.com>
2022-05-10 03:25:03 +0900

    add id query on survey create

--- frontend/src/pages/Respond.jsx
+++ frontend/src/pages/Respond.jsx
@@ -1,9 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import Form from "../modules/Form";
 import { template_list } from "../modules/Templates";
 import "../styles/Respond.css";
 
 function Respond() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const [response, setResponse] = useState();
   const onSubmitClick = () => {
     // submit the response
@@ -22,6 +25,15 @@ function Respond() {
       alert("필수 응답문항을 모두 답해주세요.");
     }
   };
+
+  useEffect(() => {
+    const id = searchParams.get("id");
+    if (id) {
+      //
+    } else {
+      navigate("/");
+    }
+  }, [searchParams]);
   return (
     <div className="respond-container">
       respond~~
