333cbb45c92a3b87e3d2d6b90661849eeb098253
Jeonghyeon Kim <powergee101@gmail.com>
2022-05-31 01:41:30 +0900

    Enlarge input data size of Nest server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
