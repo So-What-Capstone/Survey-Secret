6ca52e6e69568bc88dbb56a1d7ff613f7da485ee
moran991231 <moran991231@naver.com>
2022-05-31 23:56:55 +0900

    doing ResultList

---
+++ frontend/src/modules/RepresentativeQuestionSelector.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../styles/ResultList.scss";
+import { Select } from "antd";
+import PropTypes from "prop-types";
+const { Option } = Select;
+export default function RepresentativeQ({
+  questions,
+  representative,
+  setRepresentative,
+}) {
+  const handleChange = (value) => {
+    setRepresentative(value);
+  };
+
+  return (
+    <div className="reprentative-con">
+      <Select
+        placeholder="대표문항을 선택해주세요."
+        value={representative ? representative : null}
+        style={{
+          width: "100%",
+          marginTop: "0.5rem",
+          marginBottom: "0.5rem",
+        }}
+        onChange={handleChange}
+      >
+        {Object.keys(questions).map((v, i) => (
+          <Option key={i} value={v}>
+            {questions[v]}
+          </Option>
+        ))}
+      </Select>
+    </div>
+  );
+}
+
+RepresentativeQ.propTypes = {
+  questions: PropTypes.shape(PropTypes.any),
+  representative: PropTypes.string,
+  setRepresentative: PropTypes.func,
+};
